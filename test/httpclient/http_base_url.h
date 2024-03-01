4f414cd2253f9b85b0c58cd5d8b651872e640f92
Juan Cruz Viotti <jv@jviotti.com>
2024-02-29 21:32:57 -0400

    Rename env var for server test URL (#69)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/httpclient/http_base_url.h
+++ test/httpclient/http_base_url.h
@@ -5,17 +5,17 @@
 #include <iostream> // std::cerr
 #include <string>   // std::string
 
-static const char *const SOURCEMETA_HYDRA_TEST_BASE_URL =
-    std::getenv("SOURCEMETA_HYDRA_TEST_BASE_URL");
+static const char *const SOURCEMETA_HYDRA_TEST_SERVER_BASE_URL =
+    std::getenv("SOURCEMETA_HYDRA_TEST_SERVER_BASE_URL");
 
 inline auto HTTP_BASE_URL() -> const std::string & {
-  if (!SOURCEMETA_HYDRA_TEST_BASE_URL) {
-    std::cerr
-        << "Missing environment variable: SOURCEMETA_HYDRA_TEST_BASE_URL\n";
+  if (!SOURCEMETA_HYDRA_TEST_SERVER_BASE_URL) {
+    std::cerr << "Missing environment variable: "
+                 "SOURCEMETA_HYDRA_TEST_SERVER_BASE_URL\n";
     std::exit(1);
   }
 
-  static std::string BASE_URL{SOURCEMETA_HYDRA_TEST_BASE_URL};
+  static std::string BASE_URL{SOURCEMETA_HYDRA_TEST_SERVER_BASE_URL};
   std::cerr << "Test HTTP server base URL: " << BASE_URL << "\n";
   return BASE_URL;
 }
