fc7460c24b58f592fb68fca9776f402946afe791
Juan Cruz Viotti <jv@jviotti.com>
2024-03-06 11:27:00 -0400

    Test empty responses on methods other than `HEAD` (#77)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/httpclient/stream_1_1_test.cc
+++ test/httpclient/stream_1_1_test.cc
@@ -44,6 +44,90 @@ TEST(HTTP_Stream_1_1, no_callbacks_head) {
   EXPECT_EQ(status, sourcemeta::hydra::http::Status::OK);
 }
 
+TEST(HTTP_Stream_1_1, get_empty) {
+  sourcemeta::hydra::http::ClientStream request{std::string{HTTP_BASE_URL()} +
+                                                "/empty"};
+  request.method(sourcemeta::hydra::http::Method::GET);
+
+  std::vector<sourcemeta::hydra::http::Status> statuses;
+  std::map<std::string, std::string> headers;
+  std::ostringstream body;
+
+  request.on_data(
+      [&statuses, &body](const sourcemeta::hydra::http::Status status,
+                         std::span<const std::uint8_t> buffer) noexcept {
+        statuses.push_back(status);
+        for (const auto byte : buffer) {
+          body << static_cast<char>(byte);
+        }
+      });
+
+  request.on_header([&statuses, &headers](
+                        const sourcemeta::hydra::http::Status status,
+                        std::string_view key, std::string_view value) noexcept {
+    statuses.push_back(status);
+    headers.emplace(key, value);
+  });
+
+  const auto status{request.send().get()};
+
+  // Status
+  EXPECT_EQ(status, sourcemeta::hydra::http::Status::OK);
+  EXPECT_FALSE(statuses.empty());
+  EXPECT_TRUE(std::all_of(statuses.cbegin(), statuses.cend(), [](auto value) {
+    return value == sourcemeta::hydra::http::Status::OK;
+  }));
+
+  // Headers
+  EXPECT_TRUE(headers.contains("content-length"));
+  EXPECT_EQ(headers.at("content-length"), "0");
+
+  // Body
+  EXPECT_EQ(body.str(), "");
+}
+
+TEST(HTTP_Stream_1_1, post_empty) {
+  sourcemeta::hydra::http::ClientStream request{std::string{HTTP_BASE_URL()} +
+                                                "/empty"};
+  request.method(sourcemeta::hydra::http::Method::POST);
+
+  std::vector<sourcemeta::hydra::http::Status> statuses;
+  std::map<std::string, std::string> headers;
+  std::ostringstream body;
+
+  request.on_data(
+      [&statuses, &body](const sourcemeta::hydra::http::Status status,
+                         std::span<const std::uint8_t> buffer) noexcept {
+        statuses.push_back(status);
+        for (const auto byte : buffer) {
+          body << static_cast<char>(byte);
+        }
+      });
+
+  request.on_header([&statuses, &headers](
+                        const sourcemeta::hydra::http::Status status,
+                        std::string_view key, std::string_view value) noexcept {
+    statuses.push_back(status);
+    headers.emplace(key, value);
+  });
+
+  const auto status{request.send().get()};
+
+  // Status
+  EXPECT_EQ(status, sourcemeta::hydra::http::Status::OK);
+  EXPECT_FALSE(statuses.empty());
+  EXPECT_TRUE(std::all_of(statuses.cbegin(), statuses.cend(), [](auto value) {
+    return value == sourcemeta::hydra::http::Status::OK;
+  }));
+
+  // Headers
+  EXPECT_TRUE(headers.contains("content-length"));
+  EXPECT_EQ(headers.at("content-length"), "0");
+
+  // Body
+  EXPECT_EQ(body.str(), "");
+}
+
 TEST(HTTP_Stream_1_1, get_root_foo_bar) {
   sourcemeta::hydra::http::ClientStream request{std::string{HTTP_BASE_URL()} +
                                                 "/foo/bar"};
