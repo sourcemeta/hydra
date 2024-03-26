77cfcd38fd017b787896607e3e4332202efd7dae
Juan Cruz Viotti <jv@jviotti.com>
2024-03-26 14:07:09 -0400

    Add a method to retrieve request URL path in HTTP Server
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/httpserver/include/sourcemeta/hydra/httpserver_request.h
+++ src/httpserver/include/sourcemeta/hydra/httpserver_request.h
@@ -63,6 +63,7 @@ public:
   ///   response.status(sourcemeta::hydra::http::Status::OK);
   ///   const auto host{request.header("host")};
   ///   assert(host.has_value());
+  ///   std::ostringstream result;
   ///   result << "The host is: " << host.value();
   ///   response.end(result.str());
   /// }
@@ -84,6 +85,7 @@ public:
   /// on_root(const sourcemeta::hydra::http::ServerRequest &request,
   ///         sourcemeta::hydra::http::ServerResponse &response) -> void {
   ///   response.status(sourcemeta::hydra::http::Status::OK);
+  ///   std::ostringstream result;
   ///   const auto foo{request.query("foo")};
   ///   if (foo.has_value()) {
   ///     result << "Foo: " << foo.value();
@@ -97,6 +99,29 @@ public:
   /// server.route(sourcemeta::hydra::http::Method::GET, "/", on_root);
   /// ```
   auto query(std::string_view key) const -> std::optional<std::string>;
+
+  /// Get the path of the incoming request URL. Note that the path does not
+  /// include query or fragments. For example:
+  ///
+  /// ```cpp
+  /// #include <sourcemeta/hydra/httpserver.h>
+  /// #include <sstream>
+  /// #include <cassert>
+  ///
+  /// sourcemeta::hydra::http::Server server;
+  ///
+  /// static auto
+  /// on_root(const sourcemeta::hydra::http::ServerRequest &request,
+  ///         sourcemeta::hydra::http::ServerResponse &response) -> void {
+  ///   response.status(sourcemeta::hydra::http::Status::OK);
+  ///   std::ostringstream result;
+  ///   result << request.path();
+  ///   response.end(result.str());
+  /// }
+  ///
+  /// server.route(sourcemeta::hydra::http::Method::GET, "/", on_root);
+  /// ```
+  auto path() const -> std::string;
 
 private:
   // PIMPL idiom to hide uWebSockets
