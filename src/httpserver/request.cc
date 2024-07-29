c1fd8f6d43a26f300198fb7ea51584c0a39934e4
Juan Cruz Viotti <jv@jviotti.com>
2024-07-29 13:17:48 -0400

    Improve tolerance of `If-Modified-Since` time point comparison
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/httpserver/request.cc
+++ src/httpserver/request.cc
@@ -71,9 +71,9 @@ auto ServerRequest::header_if_modified_since(
       return true;
     }
 
-    // We have seen than on Docker, time comparison can be flaky, but adding
-    // a bit of tolerance leads to more consistent behavior.
-    return (if_modified_since.value() + std::chrono::microseconds(100)) <
+    // Time comparison can be flaky, but adding a bit of tolerance leads
+    // to more consistent behavior.
+    return (if_modified_since.value() + std::chrono::milliseconds(1)) <
            last_modified;
     // If there is an error parsing the `If-Modified-Since` timestamp, don't
     // abort, but lean on the safe side: the requested resource has been
