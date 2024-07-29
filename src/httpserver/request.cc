28237bc3a77b649b4c8593ffda3c4c6d57cd0fa6
Juan Cruz Viotti <jv@jviotti.com>
2024-07-29 12:47:40 -0400

    Add tolerance to `If-Modified-Since` comparison for Docker
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/httpserver/request.cc
+++ src/httpserver/request.cc
@@ -71,7 +71,10 @@ auto ServerRequest::header_if_modified_since(
       return true;
     }
 
-    return if_modified_since.value() < last_modified;
+    // We have seen than on Docker, time comparison can be flaky, but adding
+    // a bit of tolerance leads to more consistent behavior.
+    return (if_modified_since.value() + std::chrono::microseconds(100)) <
+           last_modified;
     // If there is an error parsing the `If-Modified-Since` timestamp, don't
     // abort, but lean on the safe side: the requested resource has been
     // modified
